4f355cebb2ec5b8ff02a8dafd6ef78d3ebb51ea2
Nasik <nasik2ms@gmail.com>
2020-05-20 14:16:03 +0530

    More cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,6 @@
 import '../scss/app.scss';
 
 // loads the Bootstrap jQuery plugins
-import 'bootstrap-sass/assets/javascripts/bootstrap/transition.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap/alert.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/dropdown.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap/modal.js';
 import 'jquery'
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
