d4a886b0bc5b1f0c22478f5e04d297e4bd19c769
Nasik <nasik2ms@gmail.com>
2020-05-22 04:50:15 +0530

    Modification to the relationship on migration

--- src/Migrations/Version20200521162459.php
+++ src/Migrations/Version20200521162459.php
@@ -23,7 +23,7 @@ final class Version20200521162459 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE discount (id INT AUTO_INCREMENT NOT NULL, invoice_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, type SMALLINT NOT NULL, percentage SMALLINT NOT NULL, amount INT NOT NULL, INDEX IDX_E1E0B40E2989F1FD (invoice_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE discount ADD CONSTRAINT FK_E1E0B40E2989F1FD FOREIGN KEY (invoice_id) REFERENCES invoice (id)');
+        $this->addSql('ALTER TABLE discount ADD CONSTRAINT FK_E1E0B40E2989F1FD FOREIGN KEY (invoice_id) REFERENCES invoice (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
