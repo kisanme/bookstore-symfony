11d13cf842e3d5151fec7745a82ed2b5ae4f4495
Nasik <nasik2ms@gmail.com>
2020-05-23 16:13:24 +0530

    Add to cart tests

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,10 +2,22 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Invoice;
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+    }
+
+
     private $categories = [
         0, 1, // Correct categories
         4, 5  // Invalid categories
@@ -45,11 +57,34 @@ class HomeControllerTest extends WebTestCase
 
         $firstBook = $crawler
             ->filter('article.book > a')
-            ->first()
+            ->last()
         ;
         $firstBookName = $firstBook->text();
         $crawler = $client->click($firstBook->link());
         $this->assertEquals($firstBookName, $crawler->filter('h1')->first()->text());
         $this->assertSelectorTextContains('a.add-to-cart', 'Add to Cart');
+    }
+
+    public function testAddRemoveCartItem()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $firstBook = $crawler->filter('article.book')->first()->text();
+
+        if ( strpos($firstBook, 'Add to Cart') != false ) {
+            $client->request('POST', '/add-to-cart/1');
+            $this->assertCartHasItem($client);
+        } else {
+            $client->request('POST', '/remove-from-cart/1');
+        }
+
+        $this->assertResponseIsSuccessful();
+
+    }
+
+    private function assertCartHasItem($client) {
+        $homepage = $client->request('GET', '/');
+        $cart = $homepage->filter('.top-cart')->first()->text();
+        $this->assertStringContainsStringIgnoringCase('lkr', $cart);
     }
 }
